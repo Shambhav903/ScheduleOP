bb98bf7c6d50a2e37f64a705b3633df481425156
Aayush Shrestha <03aayush10@gmail.com>
2023-10-30 21:38:56 +0545

    Background Probability Distribution
    Calculation of basic random sample space and the probability distribution

---
+++ sample_prob.py
@@ -0,0 +1,41 @@
+
+import math as m
+
+#possible combination in one slot/ Atom
+x = 7*3*3
+
+# We have 54 slots
+sample_space = x**54
+print(sample_space)
+
+# valid gene
+# for one student group routine we have 6 subject 
+# Each can take 2 slots out of 18
+subject = m.comb(18,2)*m.comb(16,2)*m.comb(14,2)*m.comb(12,2)*m.comb(10,2)*m.comb(8,2)
+
+# We repeat the process for 3 student group
+subject = subject**3 
+
+# For class room there are 6 possible permutations for each 3 slots
+# and we have 54/3 = 18 slots
+classroom = 6**18
+
+# same case for student group
+std_group = classroom
+
+# so valid combinations 
+valid_comb = subject * classroom * std_group
+
+print(valid_comb)
+
+#so our possibility of success in random gene generation is
+p = valid_comb/sample_space
+print(p)
+
+#and if X is the variable denoting number of attempt till success
+# P(X = x) = p(1-p)^(x-1)
+# SO likely geometric distribution may be
+
+
+
+
